972ab352b9668b24f668842aa56025ab9769ab4e
99Raul <rauljr96@gmail.com>
2023-06-01 22:59:03 -0700

    rename file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
+// import { resolve } from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
